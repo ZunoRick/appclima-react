bc22ea22200f8da0133f9d6951bba258523bdee1
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-05-16 19:48:39 -0500

    Agregando el provider

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react"
+import useClima from "../hooks/useClima"
 
 const Formulario = () => {
+  const [alerta, setAlerta] = useState('')
+  const { busqueda, datosBusqueda, consultarClima } =  useClima()
+  const { ciudad, pais } = busqueda
+
+  const handleSubmit = e =>{
+    e.preventDefault()
+
+    if (Object.values(busqueda).includes('')) {
+      setAlerta('Todos los campos son obligatorios')
+      return
+    }
+
+    consultarClima(busqueda)
+  }
+
   return (
     <div className="contenedor">
-      <form>
+      {alerta && <p>{alerta}</p>}
+      <form
+        onSubmit={handleSubmit}
+      >
         <div className="campo">
           <label htmlFor="ciudad">Ciudad</label>
           <input 
             type="text" 
             name="ciudad" 
             id="ciudad" 
+            onChange={datosBusqueda}
+            value={ciudad}
           />
         </div>
 
@@ -17,6 +39,8 @@ const Formulario = () => {
           <select 
             name="pais" 
             id="pais"
+            onChange={datosBusqueda}
+            value={pais}
           >
             <option value=""> Seleccione un país</option>
             <option value="US">Estados Unidos</option>
